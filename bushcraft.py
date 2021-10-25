a45bb0d155828670e3019839b89bdf90bf1406f2
MertcanYildirim078 <99068314@mydavinci.nl>
2021-10-25 11:28:12 +0200

    'Bushcraft_opdr1'

---
+++ bushcraft.py
@@ -0,0 +1,8 @@
+afstand = 244 #km
+liter = 12 #km
+dieselprijs = float(input('voer de dieselprijs in: '))
+persoonk = 8
+afstandkosten = afstand / liter
+
+totaal = (afstandkosten * dieselprijs) + (persoonk * 5) * 2 
+print(totaal, 'euro. De kosten voor de heen en weer reis.')
